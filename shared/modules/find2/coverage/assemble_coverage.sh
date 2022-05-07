c5c20f952432ebb1b000b4ee43ca1261a3a7e6fa
Thomas E Wilson <wilsonte@gl-login1.arc-ts.umich.edu>
2022-05-07 10:30:14 -0400

    suppress svCapture find:coverage

--- shared/modules/find2/coverage/assemble_coverage.sh
+++ shared/modules/find2/coverage/assemble_coverage.sh
@@ -5,8 +5,13 @@
 # outputs:
 #     $COVERAGE_PREFIX.rds
 
+# coverage map only applies to whole genomic SV analysis
+if [[ "$TARGETS_BED" = "" || "$TARGETS_BED" = "NA" || "$TARGETS_BED" = "null" ]]; then
+
 echo "assembling composite coverage file"
 Rscript $ACTION_DIR/coverage/assemble_coverage.R
 checkPipe
 
 echo "done"
+
+fi
