53e02c41ccd0fd931ed87b661e35136176b8b12a
Thomas E Wilson <62477613+wilsonte-umich@users.noreply.github.com>
2022-10-23 10:39:08 -0400

    Update adjustCells.js

--- shiny/apps/scCNV/modules/appSteps/adjustCells/adjustCells.js
+++ shiny/apps/scCNV/modules/appSteps/adjustCells/adjustCells.js
@@ -1,14 +1,11 @@
-cellKeepReject = function(prefix, cell_id, override){
-console.log(prefix)
-console.log(cell_id)
-console.log(override)
+var cellKeepReject = function(prefix, cell_id, override){
     Shiny.setInputValue(
         prefix + 'cellKeepReject',
         {cell_id: cell_id, override: override},
         {priority: "event"}
     );
 };
-cellSetModalCN = function(prefix, cell_id, inputId){
+var cellSetModalCN = function(prefix, cell_id, inputId){
     Shiny.setInputValue(
         prefix + 'cellSetModalCN',
         {cell_id: cell_id, value: $("#" + inputId).val()},
